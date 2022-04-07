aaf388816d377977d9a54d864f67ee29a988c0c5
Max <maxime-obry@hotmail.fr>
2022-04-07 15:42:28 +0200

    end of exercice1

--- Header/Exercice1.h
+++ Header/Exercice1.h
@@ -6,4 +6,6 @@
 #define TP2C___EXERCICE1_H
 
 void Exercice1();
+void first_function();
+void show_pointeur(int *n);
 #endif //TP2C___EXERCICE1_H
--- Src/Exercice1.cpp
+++ Src/Exercice1.cpp
@@ -6,7 +6,43 @@
 
 void Exercice1 (){
 
+    std::cout << "---------- Exercice 1 --------------" << std::endl;
+    first_function();
+}
 
+void show_pointeur (int *n){
 
+    std::cout << "pointeur : "<< n << std::endl;
+    std::cout << "adresse : "<< &n << std::endl;
+    std::cout << "valeur : "<< *n << std::endl;
+}
+
+void construct (int* const n) {
+
+    *n = 42;
 
 }
+
+void destruct(const int* n){
+    delete n;
+    n = nullptr;
+}
+
+void first_function (){
+
+    int n = 5;
+    int &r = n;
+    int *p = &n;
+    std::cout <<  *p << "," << &n <<","<< &r << std::endl;
+    show_pointeur(&n);
+    construct(&n);
+    construct(&r);
+    construct(p);
+    destruct(&n);
+    destruct(&r);
+    destruct(p);
+}
+
+
+
+
